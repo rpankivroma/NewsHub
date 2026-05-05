f64de3f6e36f780c3c75fe177388e99466943a5d
rpankivroma <pankiv.roma@gmail.com>
2026-05-05 17:44:41 +0300

    Update __init__.py

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,8 +1,22 @@
-from .user import User, UserCreate, UserVerify
+from .user import User, UserCreate, UserVerify, UserLogin, UserResendCode, ForgotPasswordRequest, ResetPasswordRequest, UserUpdate
 from .category import Category, CategoryCreate
-from .article_schema import Article, ArticleCreate
+from .article_schema import Article, ArticleCreate, ArticleUpdate
 from .comment import Comment, CommentCreate
+from .submission_schema import Submission, SubmissionCreate, SubmissionUpdate, SubmissionUserUpdate
 from .token import Token, TokenData
+from .donation_settings import DonationSettings, DonationSettingsCreate
+from .donation import Donation, DonationCreate
+from .about_page import AboutPage, AboutPageCreate
 
 # This allows importing all schemas from backend.app.schemas
-__all__ = ["User", "UserCreate", "UserVerify", "Category", "CategoryCreate", "Article", "ArticleCreate", "Comment", "CommentCreate", "Token", "TokenData"]
+__all__ = [
+    "User", "UserCreate", "UserVerify", "UserLogin", "UserResendCode", "ForgotPasswordRequest", "ResetPasswordRequest", "UserUpdate",
+    "Category", "CategoryCreate", 
+    "Article", "ArticleCreate", "ArticleUpdate",
+    "Comment", "CommentCreate", 
+    "Submission", "SubmissionCreate", "SubmissionUpdate",
+    "Token", "TokenData",
+    "DonationSettings", "DonationSettingsCreate",
+    "Donation", "DonationCreate",
+    "AboutPage", "AboutPageCreate"
+]
