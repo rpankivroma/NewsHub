8172ebdc0dc3d98b20435d82027b0c41ff39fcc0
rpankivroma <pankiv.roma@gmail.com>
2026-05-05 17:43:03 +0300

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,7 +3,11 @@ from .category import Category
 from .article_model import Article
 from .comment import Comment
 from .donation import Donation
+from .donation_settings import DonationSettings
+from .about_page import AboutPage
+from .submission_model import Submission
+from .analytics import AdminLog, Visit
 from ..db.database import Base
 
 # This allows importing all models from backend.app.models
-__all__ = ["User", "Category", "Article", "Comment", "Donation", "Base"]
+__all__ = ["User", "Category", "Article", "Comment", "Donation", "DonationSettings", "AboutPage", "Submission", "AdminLog", "Visit", "Base"]
