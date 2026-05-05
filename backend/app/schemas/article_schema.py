87ed5916f109f58b2e1d83a7262ba5690e59e606
rpankivroma <pankiv.roma@gmail.com>
2026-05-05 17:44:44 +0300

    Update article_schema.py

--- backend/app/schemas/article_schema.py
+++ backend/app/schemas/article_schema.py
@@ -13,12 +13,26 @@ class ArticleBase(BaseModel):
 class ArticleCreate(ArticleBase):
     pass
 
+class ArticleUpdate(BaseModel):
+    title: Optional[str] = None
+    excerpt: Optional[str] = None
+    content: Optional[str] = None
+    image_url: Optional[str] = None
+    is_featured: Optional[bool] = None
+    category_id: Optional[int] = None
+
 class Article(ArticleBase):
     id: int
-    author_id: int
+    author_id: Optional[int] = None
     likes: int
     dislikes: int
+    views: int
     created_at: datetime
     
+    # Extra fields for frontend
+    author: Optional[str] = None
+    category: Optional[str] = None
+    date: Optional[str] = None
+
     class Config:
         from_attributes = True
