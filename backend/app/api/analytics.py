5d7d7b19fce63f9ec705c9a20a322ef04a69c6d6
rpankivroma <pankiv.roma@gmail.com>
2026-05-13 11:42:31 +0300

    Update analytics.py

--- backend/app/api/analytics.py
+++ backend/app/api/analytics.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 from .. import schemas
 from ..db.database import get_db
-from ..repositories.analytics_repository import AnalyticsRepository
+from ..services.analytics_service import AnalyticsService
 
 router = APIRouter(prefix="/analytics", tags=["analytics"])
 
 @router.post("/track-visit")
 def track_visit(request: Request, visit: schemas.VisitCreate, db: Session = Depends(get_db)):
     ip_address = request.client.host if request.client else None
-    return AnalyticsRepository.create_visit(db, visit, ip_address)
+    return AnalyticsService.track_visit(db, visit, ip_address)
