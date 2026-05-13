ea216d09c0d87351aa7a936410755fbe9e31c795
rpankivroma <pankiv.roma@gmail.com>
2026-05-13 11:42:55 +0300

    Update security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("JWT_SECRET")
 ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "720"))
 
 if not SECRET_KEY:
     raise ValueError("JWT_SECRET environment variable is not set")
