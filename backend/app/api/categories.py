c0617f8ee72ae7e643376555f3e50c19cd6d6c59
rpankivroma <pankiv.roma@gmail.com>
2026-05-13 11:42:41 +0300

    Update categories.py

--- backend/app/api/categories.py
+++ backend/app/api/categories.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import Session
 from typing import List
 from .. import schemas
 from ..db.database import get_db
-from ..repositories.category_repository import CategoryRepository
+from ..services.category_service import CategoryService
 
 router = APIRouter(prefix="/categories", tags=["categories"])
 
 @router.get("", response_model=List[schemas.Category])
 def read_categories(db: Session = Depends(get_db)):
-    return CategoryRepository.get_all(db)
+    return CategoryService.get_all_categories(db)
